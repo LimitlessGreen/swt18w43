3194b60444c592a82d66a3d331bb572afec71bc2
Kygri <s2307505@mail.zih.tu-dresden.de>
2018-11-13 17:18:46 +0100

    [Prototyp] files for userdepartment

---
+++ src/main/java/bioladen/user/UserAcc.java
@@ -0,0 +1,14 @@
+package bioladen.user;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+
+import org.salespointframework.useraccount.UserAccount;
+
+@Entity
+public class UserAcc extends org.salespointframework.useraccount.UserAccount {
+
+
+}
---
+++ src/main/java/bioladen/user/UserAccController.java
@@ -0,0 +1,16 @@
+package bioladen.user;
+
+import javax.validation.Valid;
+
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.util.Assert;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class UserAccController {
+
+}
