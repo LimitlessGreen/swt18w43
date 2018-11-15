588c27f0d39a5d9bb6b93b03a2c5990f7b41e162
akulisch <adrian.kulisch@mailbox.tu-dresden.de>
2018-11-15 22:55:00 +0100

    [Product] Added Distributor and correlated classes

---
+++ src/main/java/bioladen/product/distributor/Distributor.java
@@ -0,0 +1,45 @@
+package bioladen.product.distributor;
+
+import javax.persistence.AttributeOverride;
+import javax.persistence.Column;
+import javax.persistence.EmbeddedId;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+import org.salespointframework.core.AbstractEntity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * A distributor.
+ * 
+ * @author Adrian Kulisch
+ */
+
+@Entity
+@Table(name = "DISTRIBUTOR")
+public class Distributor extends AbstractEntity<DistributorIdentifier> {
+	
+	@EmbeddedId
+	@AttributeOverride(name = "id", column = @Column(name = "DISTRIBUTOR_ID")) //
+	private DistributorIdentifier distributorIdentifier = new DistributorIdentifier();
+	
+	private @Getter @Setter String name;
+	private @Getter @Setter String email;
+	private @Getter @Setter String contactName;
+	private @Getter @Setter String phone;
+	
+	public Distributor(DistributorIdentifier distributorIdentifier, String name, String email, String contactName, String phone) {
+		this.distributorIdentifier = distributorIdentifier;
+		this.name = name;
+		this.email = email;
+		this.contactName = contactName;
+		this.phone = phone;
+	}
+	
+	@Override
+	public DistributorIdentifier getId() {
+		return distributorIdentifier;
+	}
+}
---
+++ src/main/java/bioladen/product/distributor/DistributorRepository.java
@@ -0,0 +1,10 @@
+package bioladen.product.distributor;
+
+import org.springframework.data.repository.CrudRepository;
+
+/**
+ * @author Adrian Kulisch
+ */
+public interface DistributorRepository extends CrudRepository<Distributor, Long> {
+
+}
