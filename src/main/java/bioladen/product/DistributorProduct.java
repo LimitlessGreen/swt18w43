efc616f0f1cebc69af1a058c07419952c02e1544
LimitlessGreen <s8931012@mail.zih.tu-dresden.de>
2018-11-16 15:17:01 +0100

    [DistributorProduct] added plain 'DistributorProduct'

--- src/main/java/bioladen/product/DistributorProduct.java
+++ src/main/java/bioladen/product/DistributorProduct.java
@@ -3,9 +3,24 @@
  */
 package bioladen.product;
 
+import org.salespointframework.core.AbstractEntity;
+
+import javax.persistence.*;
+
 /**
  * @author Adrian Kulisch
  */
-public class DistributorProduct {
-	
+
+@Entity
+@Table(name = "DISTRIBUTOR_PRODUCT")
+public class DistributorProduct extends AbstractEntity<DistributorProductIdentifier> {
+
+	@EmbeddedId
+	@AttributeOverride(name = "id", column = @Column(name = "DISTRIBUTOR_PRODUCT_ID")) //
+	private DistributorProductIdentifier distributorProductIdentifier = new DistributorProductIdentifier();
+
+	@Override
+	public DistributorProductIdentifier getId() {
+		return distributorProductIdentifier;
+	}
 }
