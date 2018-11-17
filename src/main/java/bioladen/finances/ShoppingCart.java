df0c90f96de418b5a1770d9238b40eefb9125e91
Evenaar <Lukas.Petzold1@mailbox.tu-dresden.de>
2018-11-17 19:46:43 +0100

    [Finances] TODO for ShoppingCart price calculation

--- src/main/java/bioladen/finances/ShoppingCart.java
+++ src/main/java/bioladen/finances/ShoppingCart.java
@@ -2,8 +2,19 @@ package bioladen.finances;
 
 import org.salespointframework.order.Cart;
 
+import javax.money.MonetaryAmount;
+
+/**
+ * @author Lukas Petzold
+ */
 public class ShoppingCart extends Cart {
 
+	/* @Override TODO: Calculation of sum of ShoppingCart
+	public MonetaryAmount getPrice() {
+		MonetaryAmount price = super.getPrice();
+		price = price.multiply(1.19);
 
+		return price;
+	} */
 
 }
