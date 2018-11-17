de495d52b190580d3a3218e0ee208b67626b11cd
akulisch <adrian.kulisch@mailbox.tu-dresden.de>
2018-11-17 14:52:47 +0100

    [Product] Added  /distributorlist page

--- src/main/java/bioladen/product/distributor/DistributorController.java
+++ src/main/java/bioladen/product/distributor/DistributorController.java
@@ -1,6 +1,10 @@
 package bioladen.product.distributor;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 @Controller
 public class DistributorController {
@@ -9,5 +13,13 @@ public class DistributorController {
 
 	DistributorController(DistributorRepository distributorRepository) {
 		this.distributorRepository = distributorRepository;
+	}
+
+	@GetMapping("/distributorlist")
+	String customerRepository (Model model) {
+		List<Distributor> distributorList = distributorRepository.findAll();
+		model.addAttribute("distributorList", distributorList);
+
+		return "distributorlist";
 	}
 }
--- src/main/java/bioladen/product/distributor/DistributorRepository.java
+++ src/main/java/bioladen/product/distributor/DistributorRepository.java
@@ -2,9 +2,11 @@ package bioladen.product.distributor;
 
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.ArrayList;
+
 /**
  * @author Adrian Kulisch
  */
 public interface DistributorRepository extends CrudRepository<Distributor, DistributorIdentifier> {
-
+	public ArrayList<Distributor> findAll();
 }
