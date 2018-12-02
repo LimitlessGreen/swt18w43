6710308d480f2844d8e5215e20960a8899f1f492
akulisch <adrian.kulisch@mailbox.tu-dresden.de>
2018-12-02 14:06:35 +0100

    [Product] Added Full Name to Organization Enum

--- src/main/java/bioladen/product/Organization.java
+++ src/main/java/bioladen/product/Organization.java
@@ -1,11 +1,19 @@
 package bioladen.product;
 
+import lombok.Getter;
+
 public enum Organization {
-	BIOLAND,
-	BIOPARK,
-	DEMETER,
-	ECOVIN,
-	EG_BIO,
-	GAEA,
-	NATURLAND;
+	BIOLAND("Bioland"),
+	BIOPARK("Biopark e.V."),
+	DEMETER("Demeter e.V."),
+	ECOVIN("ECOVIN Bundesverband Ökologischer Weinbau e.V."),
+	EG_BIO("BIO nach EG-Öko-Verordnung"),
+	GAEA("Gäa e.V. – Vereinigung ökoloschier Landbau"),
+	NATURLAND("Naturland – Verband für ökologischen Landbau e.V.");
+
+	private final @Getter String name;
+
+	Organization(String name) {
+		this.name = name;
+	}
 }
