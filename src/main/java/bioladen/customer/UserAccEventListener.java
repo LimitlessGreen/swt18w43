229aa211685bf4dabe76029c48b774aaf5d043e3
Kygri <s2307505@mail.zih.tu-dresden.de>
2018-12-07 18:47:50 +0100

    [Customer][WIP] modification methods for acc

--- src/main/java/bioladen/customer/UserAccEventListener.java
+++ src/main/java/bioladen/customer/UserAccEventListener.java
@@ -22,30 +22,19 @@ public class UserAccEventListener {
 	@EventListener
 	public void listenCustomerEvent(EntityEvent<Customer> event) {
 		Customer customer = event.getEntity();
+		Customer oldCustomer = event.getEntityBeforeModified().orElse(null);
 
 		switch (event.getEventLevel()) {
 			case CREATED:
-				if (customer.isCustomerType(CustomerType.STAFF)) {
-					this.save(customer, "ROLE_STAFF");
-				} else if (customer.isCustomerType(CustomerType.MANAGER)) {
-					this.save(customer, "ROLE_MANAGER");
-				}
+				this.create(customer);
 				break;
-			case DELETED: //TODO in new Salespoint version delete method: userAccount.delete(userAccountManager.findByUsername(customer.getEmail()).getAll());
-				if (customer.isCustomerType(CustomerType.STAFF)) {
-					userAccountManager.disable(userAccountManager.findByUsername(customer.getEmail()).get().getId());
-				} else if (customer.isCustomerType(CustomerType.MANAGER)) {
-					userAccountManager.disable(userAccountManager.findByUsername(customer.getEmail()).get().getId());
-				}
+			case DELETED: //new Salespoint delete-method:
+				this.delete(customer); //userAccount.delete(userAccountManager.findByUsername(customer.getEmail()).getAll());
 				break;
 
 			case MODIFIED:
-				if (customer.isCustomerType(CustomerType.STAFF)) {
-
-				} else if (customer.isCustomerType(CustomerType.MANAGER)) {
-
-				}
-				break; //TODO Customer modified
+				this.modified(oldCustomer, customer);
+				break;
 			default:
 				break;
 		}
@@ -58,6 +47,65 @@ public class UserAccEventListener {
 			userAccountManager.save(userAccount);
 		} else {
 			userAccountManager.enable(userAccountManager.findByUsername(customer.getEmail()).get().getId());
+		}
+	}
+
+	private void delete(Customer customer){
+		if (customer.isCustomerType(CustomerType.STAFF)) {
+			userAccountManager.disable(userAccountManager.findByUsername(customer.getEmail()).get().getId());
+		} else if (customer.isCustomerType(CustomerType.MANAGER)) {
+			userAccountManager.disable(userAccountManager.findByUsername(customer.getEmail()).get().getId());
+		}
+	}
+
+	private void create(Customer customer){
+		if (customer.isCustomerType(CustomerType.STAFF)) {
+			this.save(customer, "ROLE_STAFF");
+		} else if (customer.isCustomerType(CustomerType.MANAGER)) {
+			this.save(customer, "ROLE_MANAGER");
+		}
+	}
+
+	private void update(Customer oldCustomer, Customer customer, String role){
+		if (!userAccountManager.findByUsername(customer.getEmail()).isPresent()) {
+			UserAccount userAccount = userAccountManager.create(customer.getEmail(),
+					String.valueOf(userAccountManager.findByUsername(oldCustomer.getEmail()).get().getPassword()),
+					Role.of(role));
+			userAccount.setEmail(customer.getEmail());
+			userAccountManager.save(userAccount);
+		} else {
+			userAccountManager.enable(userAccountManager.findByUsername(customer.getEmail()).get().getId());
+		}
+
+	}
+
+	private void modified(Customer oldCustomer, Customer customer){
+		if (!oldCustomer.getCustomerType().equals(customer.getCustomerType())){
+			if (oldCustomer.isCustomerType(CustomerType.STAFF) && customer.isCustomerType(CustomerType.MANAGER)){
+				if(!oldCustomer.getEmail().equals(customer.getEmail())){
+					this.update(oldCustomer, customer, "ROLE_MANAGER");
+				} else {
+					this.update(customer, customer, "ROLE_MANAGER");
+				}
+			} else if (oldCustomer.isCustomerType(CustomerType.MANAGER) && customer.isCustomerType(CustomerType.STAFF)){
+				if(!oldCustomer.getEmail().equals(customer.getEmail())){
+					this.update(oldCustomer, customer, "ROLE_STAFF");
+				} else {
+					this.update(customer, customer, "ROLE_STAFF");
+				}
+			}
+			if ((oldCustomer.isCustomerType(CustomerType.MANAGER) ||
+					oldCustomer.isCustomerType(CustomerType.STAFF)) &&
+					(customer.isCustomerType(CustomerType.HOUSE_CUSTOMER) ||
+							customer.isCustomerType(CustomerType.MAJOR_CUSTOMER))){
+				this.delete(oldCustomer);
+
+			} else if ((oldCustomer.isCustomerType(CustomerType.HOUSE_CUSTOMER) ||
+					oldCustomer.isCustomerType(CustomerType.MAJOR_CUSTOMER)) &&
+					(customer.isCustomerType(CustomerType.MANAGER) ||
+							customer.isCustomerType(CustomerType.STAFF))){
+				this.create(customer);
+			}
 		}
 	}
 }
