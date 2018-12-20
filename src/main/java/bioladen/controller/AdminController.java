72cf036af4f7d9a60247b3402db6a248a492e1eb
Kygri <s2307505@mail.zih.tu-dresden.de>
2018-12-20 18:42:56 +0100

    [Newsletter] updated
    Co-authored-by: Evenaar <Lukas.Petzold1@mailbox.tu-dresden.de>

--- src/main/java/bioladen/controller/AdminController.java
+++ src/main/java/bioladen/controller/AdminController.java
@@ -3,6 +3,7 @@ package bioladen.controller;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -20,7 +21,29 @@ public class AdminController {
 
 	@PostMapping("/newsletter")
 	public String newsletter(@RequestParam String message) {
-
+		//TODO write Newsletter
 		return "admin";
+	}
+
+	@RequestMapping("/cancel_newsletter")
+	public String cancelNewsletter() {
+		return "cancel_newsletter";
+	}
+
+	@PostMapping("/cancel_newsletter")
+	public String cancelledNewsletter(@RequestParam String email,
+									  Model model) {
+		model.addAttribute("errorCancel",true);
+		//TODO cancel Newsletter
+
+		return "cancel_newsletter";
+	}
+
+	@PostMapping("register_newsletter")
+	public String registerNewsletter(@RequestParam String email,
+									 Model model) {
+		model.addAttribute("errorRegister",true);
+		//TODO register Newsletter
+		return "welcome";
 	}
 }
