76a3e6296eae70eb5aae54c1a56b800f7ea105ac
Kygri <s2307505@mail.zih.tu-dresden.de>
2018-12-21 18:29:52 +0100

    [Customer] added majorCustomer & houseCustomer in Initializer

--- src/main/java/bioladen/customer/CustomerDataInitializer.java
+++ src/main/java/bioladen/customer/CustomerDataInitializer.java
@@ -31,6 +31,16 @@ public class CustomerDataInitializer implements DataInitializer {
 		Customer staff = new Customer("Berta", "Bunt", "bertabunt@bio.de", Sex.FEMALE, CustomerType.STAFF);
 		staff.setStreet("Bergstraße 69, 57612 Busenhausen");
 
+		Customer majorCustomer = new Customer("Hilde", "Gard", "garden@obst.de", Sex.FEMALE, CustomerType.MAJOR_CUSTOMER);
+		majorCustomer.setStreet("Hof 4, 06862 Hundeluft");
+		majorCustomer.setPhone("012412515");
+
+		Customer houseCustomer = new Customer("Manfred", "Jungfleisch", "jungfleisch@manfred.de",
+				Sex.MALE, CustomerType.HOUSE_CUSTOMER);
+		houseCustomer.setStreet("Holzweg 108, 63589 Linsengericht");
+		houseCustomer.setPhone("+49124214214");
+
 		customerManager.saveAll(Arrays.asList(manager, staff));
+		customerManager.saveAll(Arrays.asList(majorCustomer,houseCustomer));
 	}
 }
