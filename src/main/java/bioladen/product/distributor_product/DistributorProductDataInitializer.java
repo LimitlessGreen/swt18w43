60fd6983da0293bd04c833455181e2b58aec183e
akulisch <adrian.kulisch@mailbox.tu-dresden.de>
2019-01-10 22:40:27 +0100

    [Product] Changed DistributorProducts in Initializer to final

--- src/main/java/bioladen/product/distributor_product/DistributorProductDataInitializer.java
+++ src/main/java/bioladen/product/distributor_product/DistributorProductDataInitializer.java
@@ -22,114 +22,103 @@ public class DistributorProductDataInitializer implements DataInitializer {
 		this.distributorRepository = distributorRepository;
 	}
 
+	private static final Distributor DISTRIBUTOR1 = new Distributor(
+														"Bauer Heinze",
+														"heinze@bauern.de",
+														"Heinz Heinze",
+														"01524506154"
+													);
+	private static final Distributor DISTRIBUTOR2 = new Distributor(
+														"Fridas Hof",
+														"frida@fridas-hof.de",
+														"Frida Fritzsche",
+														"01736825268"
+													);
+
+	private static final DistributorProduct DISTRIBUTOR_PRODUCT1 = new DistributorProduct(
+																		"Kartoffeln",
+																		DISTRIBUTOR1,
+																		BigDecimal.valueOf(4.99),
+																		BigDecimal.valueOf(5),
+																		Metric.KILOGRAM,
+																		10,
+																		ProductCategory.FOOD_FRUIT_VEG,
+																		MwStCategory.REDUCED,
+																		null,
+																		Organization.BIOLAND
+																	);
+	private static final DistributorProduct DISTRIBUTOR_PRODUCT2 = new DistributorProduct(
+																		"Kartoffeln",
+																		DISTRIBUTOR2,
+																		BigDecimal.valueOf(4.49),
+																		BigDecimal.valueOf(5),
+																		Metric.KILOGRAM,
+																		15,
+																		ProductCategory.FOOD_FRUIT_VEG,
+																		MwStCategory.REDUCED,
+																		null,
+																		Organization.BIOLAND
+																	);
+	private static final DistributorProduct DISTRIBUTOR_PRODUCT3 = new DistributorProduct(
+																		"Kartoffeln",
+																		DISTRIBUTOR2,
+																		BigDecimal.valueOf(3.50),
+																		BigDecimal.valueOf(5),
+																		Metric.KILOGRAM,
+																		3,
+																		ProductCategory.FOOD_FRUIT_VEG,
+																		MwStCategory.REDUCED,
+																		null,
+																		Organization.BIOLAND
+																	);
+
+	private static final DistributorProduct DISTRIBUTOR_PRODUCT4 = new DistributorProduct(
+																		"Kakao",
+																		DISTRIBUTOR2,
+																		BigDecimal.valueOf(1.50),
+																		BigDecimal.valueOf(1),
+																		Metric.KILOGRAM,
+																		4,
+																		ProductCategory.FOOD_FRUIT_VEG,
+																		MwStCategory.REDUCED,
+																		null,
+																		Organization.BIOLAND
+																	);
+	private static final DistributorProduct DISTRIBUTOR_PRODUCT5 = new DistributorProduct(
+																		"Kakao",
+																		DISTRIBUTOR1,
+																		BigDecimal.valueOf(59.00),
+																		BigDecimal.valueOf(1),
+																		Metric.KILOGRAM,
+																		4,
+																		ProductCategory.FOOD_FRUIT_VEG,
+																		MwStCategory.REDUCED,
+																		null,
+																		Organization.BIOLAND
+																	);
+	private static final DistributorProduct DISTRIBUTOR_PRODUCT6 = new DistributorProduct(
+																		"Milch",
+																		DISTRIBUTOR2,
+																		BigDecimal.valueOf(1.50),
+																		BigDecimal.valueOf(1),
+																		Metric.LITER,
+																		6,
+																		ProductCategory.FOOD_DAIRY,
+																		MwStCategory.REDUCED,
+																		BigDecimal.valueOf(0.50),
+																		Organization.NATURLAND
+																	);
+
 	@Override
 	public void initialize() {
-		distributorRepository.save(
-				new Distributor(
-						"Bauer Heinze",
-						"heinze@bauern.de",
-						"Heinz Heinze",
-						"01524506154"
-				)
-		);
+		distributorRepository.save(DISTRIBUTOR1);
+		distributorRepository.save(DISTRIBUTOR2);
 
-		distributorRepository.save(
-				new Distributor(
-						"Fridas Hof",
-						"frida@fridas-hof.de",
-						"Frida Fritzsche",
-						"01736825268"
-				)
-		);
-
-		distributorProductCatalog.save(
-				new DistributorProduct(
-						"Kartoffeln",
-						distributorRepository.findById(1L).get(),
-						BigDecimal.valueOf(4.99),
-						BigDecimal.valueOf(5),
-						Metric.KILOGRAM,
-						10,
-						ProductCategory.FOOD_FRUIT_VEG,
-						MwStCategory.REDUCED,
-						null,
-						Organization.BIOLAND
-				)
-		);
-
-		distributorProductCatalog.save(
-				new DistributorProduct(
-						"Kartoffeln",
-						distributorRepository.findById(2L).get(),
-						BigDecimal.valueOf(4.49),
-						BigDecimal.valueOf(5),
-						Metric.KILOGRAM,
-						15,
-						ProductCategory.FOOD_FRUIT_VEG,
-						MwStCategory.REDUCED,
-						null,
-						Organization.BIOLAND
-				)
-		);
-
-		distributorProductCatalog.save(
-				new DistributorProduct(
-						"Kartoffeln",
-						distributorRepository.findById(2L).get(),
-						BigDecimal.valueOf(3.50),
-						BigDecimal.valueOf(5),
-						Metric.KILOGRAM,
-						3,
-						ProductCategory.FOOD_FRUIT_VEG,
-						MwStCategory.REDUCED,
-						null,
-						Organization.BIOLAND
-				)
-		);
-
-		distributorProductCatalog.save(
-				new DistributorProduct(
-						"Kakao",
-						distributorRepository.findById(2L).get(),
-						BigDecimal.valueOf(1.50),
-						BigDecimal.valueOf(1),
-						Metric.KILOGRAM,
-						4,
-						ProductCategory.FOOD_FRUIT_VEG,
-						MwStCategory.REDUCED,
-						null,
-						Organization.BIOLAND
-				)
-		);
-
-		distributorProductCatalog.save(
-				new DistributorProduct(
-						"Kakao",
-						distributorRepository.findById(1L).get(),
-						BigDecimal.valueOf(59.00),
-						BigDecimal.valueOf(1),
-						Metric.KILOGRAM,
-						4,
-						ProductCategory.FOOD_FRUIT_VEG,
-						MwStCategory.REDUCED,
-						null,
-						Organization.BIOLAND
-				)
-		);
-
-		distributorProductCatalog.save(
-				new DistributorProduct(
-						"Milch",
-						distributorRepository.findById(2L).get(),
-						BigDecimal.valueOf(1.50),
-						BigDecimal.valueOf(1),
-						Metric.LITER,
-						6,
-						ProductCategory.FOOD_DAIRY,
-						MwStCategory.REDUCED,
-						BigDecimal.valueOf(0.50),
-						Organization.NATURLAND
-				)
-		);
+		distributorProductCatalog.save(DISTRIBUTOR_PRODUCT1);
+		distributorProductCatalog.save(DISTRIBUTOR_PRODUCT2);
+		distributorProductCatalog.save(DISTRIBUTOR_PRODUCT3);
+		distributorProductCatalog.save(DISTRIBUTOR_PRODUCT4);
+		distributorProductCatalog.save(DISTRIBUTOR_PRODUCT5);
+		distributorProductCatalog.save(DISTRIBUTOR_PRODUCT6);
 	}
 }
